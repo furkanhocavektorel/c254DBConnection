92b35de1b39c4658310d4af2ea87043db16b80e1
furkanhocavektorel <furkanhocavektorel@gmail.com>
2025-05-26 20:53:55 +0300

    db baglantısı yapıldı

---
+++ FirstDBConnection/HepsiSuradaContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstDBConnection
+{
+    public class HepsiSuradaContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=myServerAddress;" +
+                "Database=myDataBase;" +
+                "Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
--- FirstDBConnection/Program.cs
+++ FirstDBConnection/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace FirstDBConnection
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstDBConnection
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
+            
         }
     }
 }
